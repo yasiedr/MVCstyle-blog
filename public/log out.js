a26b17c734a4fe01f445c1a99482ef94877cdca4
yasedrisian <yasedrisian-KnNIvq>
2023-04-04 13:58:06 -0400

    getting loginand log outL

---
+++ public/log out.js
@@ -0,0 +1,15 @@
+const logoutHandler = async (event) => {
+    event.preventDefault();
+  
+    const response = await fetch('/logout', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+    });
+  
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert('Failed to log out.');
+    }
+  };
+  
